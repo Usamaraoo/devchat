a80dd72b844642b2d7a3565c47efc68119ca183d
Usamaraoo <usamakaleem505@gmail.com>
2023-05-12 15:52:47 +0500

    jwt in login/regiser apies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const UserModel = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const registerUser = async (req, res) => {
   try {
@@ -8,19 +10,48 @@ const registerUser = async (req, res) => {
       res.status(500).json({ error: "user with this email already exists" });
     }
     const newUser = await UserModel.create({ name, email, password });
-    res.status(200).json(newUser);
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(password, salt);
+    await newUser.save();
+    //  return jwt
+    const payload = { user: { id: user.id } };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "7 days" },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
   } catch (error) {
     res.status(200).json({ err: "something went wrong" });
   }
 };
 const loginUser = async (req, res) => {
   try {
-    console.log('body',req.body);
     const { email, password } = req.body;
-    const user = await UserModel.findOne({ email, password });
+    const user = await UserModel.findOne({ email });
     if (user) {
-      res.status(200).json(user);
-    }else{
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Email or password incorrect" });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: '30 days' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } else {
       res.status(500).json({ error: "email or password incorrect" });
     }
   } catch (error) {
@@ -29,5 +60,5 @@ const loginUser = async (req, res) => {
 };
 module.exports = {
   registerUser,
-  loginUser
+  loginUser,
 };
