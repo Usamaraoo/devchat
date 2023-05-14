8dea13e68d49fea0ece4693129055e24125342bd
Usamaraoo <usamakaleem505@gmail.com>
2023-05-14 20:09:25 +0500

    Added test links

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { Link} from "react-router-dom";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <h3 className="text-2xl">Dashboard</h3>
+      <Link className="underline text-blue-500" to='/'>Home</Link>
+    
+    </div>
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 export default function Home() {
+  const { setAuth } = useAuth();
   return (
     <div>
-      <div>Home</div>
+      <h2 className="text-2xl">Home</h2>
+      <Link className="underline text-blue-500" to="/dashboard">
+        Dashboard
+      </Link>
+      <br />
+      <button className="px-4 px-2  bg-blue-400" onClick={() => setAuth({})}>Logout</button>
     </div>
   );
 }
