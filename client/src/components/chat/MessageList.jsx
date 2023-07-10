20b7a07ee8007923acc07341eb51dc55d8379c61
UsamaKaleemTechmate <133773857+UsamaKaleemTechmate@users.noreply.github.com>
2023-07-10 14:30:06 +0500

    chat list style update

--- client/src/components/chat/MessageList.jsx
+++ client/src/components/chat/MessageList.jsx
@@ -17,7 +17,7 @@ export default function MessageList({ messages }) {
 
   return (
     <div
-      className="overflow-y-auto mt-24 overflow-hidden pr-3"
+      className="overflow-y-auto mt-24 overflow-hidden pr-3 pb-4"
       id="style-4"
       style={{ height: "78vh" }}
     >
